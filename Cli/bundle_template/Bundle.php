951facaf7dd27015abbecfc489530c301edf5b2b
Michel Hognerud <leyou.m@gmail.com>
2014-05-17 11:35:39 +0930

    fixes and splitting core/cli

--- Cli/bundle_template/Bundle.php
+++
@@ -1,9 +0,0 @@
-<%
-namespace <?php echo $bundle['namespace'] ?>;
-
-class Bundle extends \Asgard\Core\BundleLoader {
-	public function run() {
-		<?php \Asgard\Core\App::get('hook')->trigger('Agard\CLI\generator\bundle.php', array($bundle)) ?>
-		parent::run();
-	}
-}

f13450b85e0cca99020e6c153a01629f03719962
Michel <leyou.m@gmail.com>
2015-04-17 18:43:01 +0200

    refactoring generator

--- Commands/bundle_template/html/_entity/index.php
+++
@@ -1,5 +0,0 @@
-<% foreach($<?=$entity['meta']['plural'] ?> as $<?=$entity['meta']['name'] ?>): %>
-<a href="<%=$this->url('show', ['id'=>$<?=$entity['meta']['name']?>->id])%>"><%=$<?=$entity['meta']['name']?>%></a><br>
-<% endforeach; %>
-
-<%=$paginator->render()%>
